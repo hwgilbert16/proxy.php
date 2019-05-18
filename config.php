b421cb0503d7beb6f74e0afac643a77864a60533
zerxal <44120703+hwgilbert16@users.noreply.github.com>
2019-05-18 19:04:48 -0400

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,8 @@
  * Place here any hosts for which we are to be a proxy -
  * e.g. the host on which the J2EE APIs we'll be proxying are running
  * httpbin.org
+ * 127.0.0.1
  * */
 $SETTING_ALLOWED_HOSTS = array(
-    'https://proxy-test-1.herokuapp.com','127.0.0.1', 'https://proxy-test-1.herokuapp.com' # change to restrict list to only domains you wish to allow clients to call via this proxy
+    'https://proxy-test-1.herokuapp.com','0.0.0.0, 'https://proxy-test-1.herokuapp.com' # change to restrict list to only domains you wish to allow clients to call via this proxy
 );
